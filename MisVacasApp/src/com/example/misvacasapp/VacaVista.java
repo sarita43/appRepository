6deaab1a4e10daa37616a524535bea0788a292ec
sarita43 <sara_c43@hotmail.com>
2014-12-14 13:28:52 +0100

    Vista Vacas anterior se borro inexplicable

--- MisVacasApp/src/com/example/misvacasapp/VacaVista.java
+++ MisVacasApp/src/com/example/misvacasapp/VacaVista.java
@@ -1,13 +1,9 @@
 package com.example.misvacasapp;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
 import com.example.misvacasapp.llamadaWS.LlamadaVacaWS;
 import com.example.misvacasapp.modelo.Vaca;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -15,56 +11,47 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class VacaVista extends ActionBarActivity{
-
+public class VacaVista extends ActionBarActivity {
 	private String id_vaca;
 	private String id_usuario;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_vaca_vista);
-		
-		Bundle bundle=getIntent().getExtras();
+		Bundle bundle = getIntent().getExtras();
 		id_vaca = bundle.getString("id_vaca");
 		id_usuario = bundle.getString("id_usuario");
-		
 		rellenarCamposVaca();
 	}
-	
-	private void rellenarCamposVaca(){
-		
+
+	private void rellenarCamposVaca() {
 		Thread hilo = new Thread() {
 			String res = "";
 			Gson json = new Gson();
 			LlamadaVacaWS llamada = new LlamadaVacaWS();
-			SimpleDateFormat formatoDeFecha = new SimpleDateFormat(
-					"dd-MM-yyyy");
-			
+			SimpleDateFormat formatoDeFecha = new SimpleDateFormat("dd-MM-yyyy");
 			Vaca vaca = new Vaca();
-			
+
 			public void run() {
-				
-				res=llamada.LlamadaVaca(id_vaca, id_usuario);
-				
+				res = llamada.LlamadaVaca(id_vaca, id_usuario);
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
 						vaca = json.fromJson(res, Vaca.class);
-						
-						TextView idVaca = (TextView)findViewById(R.id.idVaca);
-						idVaca.setText("ID VACA: "+vaca.getId_vaca());
-						
-						TextView raza = (TextView)findViewById(R.id.raza);
-						raza.setText("RAZA: "+ vaca.getRaza());
-						
-						TextView fechaNacimiento = (TextView)findViewById(R.id.fechaNacimiento);
-						fechaNacimiento.setText("FECHA DE NACIMIENTO: "+formatoDeFecha.format(vaca.getFecha_nacimiento()));
-						
-						TextView idMadre = (TextView)findViewById(R.id.idMadre);
-						idMadre.setText("ID MADRE: "+vaca.getId_madre());
-						
-//						ImageView imagen = (ImageView)findViewById(R.id.imageView1);
-//						imagen.setImageDrawable(vaca.getId_vaca());
+						TextView idVaca = (TextView) findViewById(R.id.idVaca);
+						idVaca.setText("ID VACA: " + vaca.getId_vaca());
+						TextView raza = (TextView) findViewById(R.id.raza);
+						raza.setText("RAZA: " + vaca.getRaza());
+						TextView fechaNacimiento = (TextView) findViewById(R.id.fechaNacimiento);
+						fechaNacimiento.setText("FECHA DE NACIMIENTO: "
+								+ formatoDeFecha.format(vaca
+										.getFecha_nacimiento()));
+						TextView idMadre = (TextView) findViewById(R.id.idMadre);
+						idMadre.setText("ID MADRE: " + vaca.getId_madre());
+						// ImageView imagen =
+						// (ImageView)findViewById(R.id.imageView1);
+						// imagen.setImageDrawable(vaca.getId_vaca());
 					}
 				});
 			}
