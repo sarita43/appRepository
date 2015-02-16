ed870e4446698a047add5dd24395dac6f07b0c47
sarita43 <sara_c43@hotmail.com>
2015-02-16 12:28:14 +0100

    comentarios y javadoc llamadaUsuarioWS

--- MisVacasApp/src/com/example/misvacasapp/llamadaWS/LlamadaUsuarioWS.java
+++ MisVacasApp/src/com/example/misvacasapp/llamadaWS/LlamadaUsuarioWS.java
@@ -9,20 +9,39 @@ import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
 import org.xmlpull.v1.XmlPullParserException;
 
+/**
+ * Clase que hace las llamadas al servicio web del usuario
+ * @author Sara Martinez Lopez
+ * */
 public class LlamadaUsuarioWS {
 
+	//Atributos
+	/** Nombre del espacio de nombres*/
 	private static final String NAMESPACE = "http://service.web";
+	/**URL donde se encuentra el servicio web*/
 //	private static String URL = "http://10.0.2.2:8090/axis2/services/UsuarioWebService?wsdl";
 
-	private static String URL = "http://81.172.100.87:8090/axis2/services/UsuarioWebService?wsdl";
+	/**Nombre del método*/
 	private static String METHOD_NAME;
+	/**SOAP Action*/
 	private static String SOAP_ACTION;
-
+	/**Solicitud del objeto soap*/
 	private static SoapObject request = null;
+	/**Contenedor soap*/
 	private static SoapSerializationEnvelope envelope = null;
+	/**Resultado de la llamada */
 	private static SoapPrimitive resultsRequestSOAP = null;
 	
-	public String LlamadaUsuario(String usuario,String contrase�a){
+	//Métodos
+	/**
+	 * Método que devuelve el usuario (como String).
+	 * El usuario que devuelve tiene que poder ser deserializado con json.formJson
+	 * Comprueba que el usuario y la contraseña coinciden 
+	 * @param usuario Id del usuario
+	 * @param contraseña Contraseña del usuario
+	 * @return String Usuario
+	 * */
+	public String LlamadaUsuario(String usuario,String contraseña){
 		String res= "";
 		
 		METHOD_NAME = "Usuario";
@@ -30,7 +49,7 @@ public class LlamadaUsuarioWS {
 
 		request = new SoapObject(NAMESPACE, METHOD_NAME);
 		request.addProperty("dni", usuario);
-		request.addProperty("contrase�a", contrase�a);
+		request.addProperty("contraseña", contraseña);
 
 		envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
 		envelope.dotNet = false;
@@ -42,23 +61,29 @@ public class LlamadaUsuarioWS {
 			res = resultsRequestSOAP.toString();
 		} catch (IOException
 				| XmlPullParserException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 		
 		return res;
 	}
 	
-	public String LlamadaUsuarioExistente(String usuario,String contrase�a){
+	/**
+	 * Método que dice si un usuario existe o no 
+	 * Si el usuario existe devuelve un 'true' y si no un 'false'
+	 * @param usuario Id del usuario
+	 * @param contraseña Contraseña del usuario
+	 * @return boolean Usuario existente o no
+	 * */
+	public boolean LlamadaUsuarioExistente(String usuario,String contraseña){
 		
-		String res= "";
+		boolean res= false;
 		
 		METHOD_NAME = "usuarioExistente";
 		SOAP_ACTION = "urn:usuarioExistente";
 
 		request = new SoapObject(NAMESPACE, METHOD_NAME);
 		request.addProperty("dni", usuario);
-		request.addProperty("contrase�a", contrase�a);
+		request.addProperty("contraseña", contraseña);
 
 		envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
 		envelope.dotNet = false;
@@ -70,20 +95,24 @@ public class LlamadaUsuarioWS {
 			res = resultsRequestSOAP.toString();
 		} catch (IOException
 				| XmlPullParserException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 		
 		return res;
 	}
 	
-	public void actualizarContrase�a(String dni,String contrase�a){
-		METHOD_NAME = "actualizarContrase�a";
-		SOAP_ACTION = "urn:actualizarContrase�a";
+	/**
+	 * Método que cambia la contraseña de un usuario
+	 * @param dni Id del usuario
+	 * @param contraseña Contraseña del usuario
+	 * */
+	public void actualizarContraseña(String dni,String contraseña){
+		METHOD_NAME = "actualizarContraseña";
+		SOAP_ACTION = "urn:actualizarContraseña";
 
 		request = new SoapObject(NAMESPACE, METHOD_NAME);
 		request.addProperty("dni", dni);
-		request.addProperty("contrase�a", contrase�a);
+		request.addProperty("contraseña", contraseña);
 
 		envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
 		envelope.dotNet = false;
@@ -98,6 +127,10 @@ public class LlamadaUsuarioWS {
 		}	
 	}
 	
+	/**
+	 * Método que elimina un usuario
+	 * @param id_usuario Id del usuario
+	 * */
 	public void eliminarUsuario(String id_usuario){
 		METHOD_NAME = "eliminarUsuario";
 		SOAP_ACTION = "urn:eliminarUsuario";
@@ -118,6 +151,16 @@ public class LlamadaUsuarioWS {
 		}	
 	}
 	
+	/**
+	 * Método que cambia los parametros del usuario: id, nombre, apellidos, dirección, pobñación, teléfono
+	 * @param dni Id del usuario
+	 * @param nombre Nombre del usuario
+	 * @param apellido1 Primer apellido del usuario
+	 * @param apellido2 Segundo apellido del usuario
+	 * @param direccion Direción del usuario
+	 * @param poblacion Población del usuario
+	 * @telefono Teléfono del usuario
+	 * */
 	public void actualizarUsuario(String dni, String nombre, String apellido1,
 			String apellido2, String direccion, String poblacion, int telefono){
 		METHOD_NAME = "actualizarUsuario";
