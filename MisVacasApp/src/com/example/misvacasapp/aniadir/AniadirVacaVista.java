2c1412cb94d682311e3142c38e18a516491f3891
sarita43 <sara_c43@hotmail.com>
2015-02-16 20:13:56 +0100

    añadir vaca OK eliminar vaca mal buscar vaca no

--- MisVacasApp/src/com/example/misvacasapp/aniadir/AniadirVacaVista.java
+++ MisVacasApp/src/com/example/misvacasapp/aniadir/AniadirVacaVista.java
@@ -10,8 +10,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
