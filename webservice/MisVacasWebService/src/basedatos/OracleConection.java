c9f646ee4207875113ec45213231b43c53dee58c
sarita43 <sara_c43@hotmail.com>
2015-02-19 12:54:11 +0100

    comentarios y javadoc

--- webservice/MisVacasWebService/src/basedatos/OracleConection.java
+++ webservice/MisVacasWebService/src/basedatos/OracleConection.java
@@ -11,24 +11,47 @@ import java.util.Date;
 
 import org.apache.naming.java.javaURLContextFactory;
 
+/**
+ * Clase que hace la conexión a la base de datos
+ * 
+ * @author Sara Martinez Lopez
+ * */
 public class OracleConection {
+	
+	//Atributos
+	/** Conexión */
 	private Connection conexion;
+	
+	//Métodos
 
-	public static void main(String[] args) {
-		OracleConection obconeccion = new OracleConection();
-		obconeccion.Conectar();
-	}
+//	public static void main(String[] args) {
+//		OracleConection obconeccion = new OracleConection();
+//		obconeccion.Conectar();
+//	}
 
+	/**
+	 * Método que devuelve la conexión 
+	 * @return Connection Conexión
+	 * */
 	public Connection getConexion() {
 		return conexion;
 	}
 
+	/**
+	 * Guarda la conexión
+	 * @param conexion
+	 * */
 	public void setConexion(Connection conexion) {
 		this.conexion = conexion;
 	}
 
+	/**
+	 * Método que hace la conexión a la base de datos
+	 * Crea la conexión con el driver jdbc. Necesitamos la IP(localhost) el puerto en el que escucha(1521) y el 
+	 * usuario y la contraseña a la base de datos
+	 * @return OracleConection 
+	 * */
 	public OracleConection Conectar() {
-
 		try {
 			InetAddress addr = InetAddress.getLocalHost();
 			String ip = addr.getHostAddress();
