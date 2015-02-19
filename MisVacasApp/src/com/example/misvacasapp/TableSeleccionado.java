a401bb7f1ac6c1f126396551d6337ddaaa7e4ac5
sarita43 <sara_c43@hotmail.com>
2015-02-19 10:41:45 +0100

    comentarios y javadoc

--- MisVacasApp/src/com/example/misvacasapp/TableSeleccionado.java
+++ MisVacasApp/src/com/example/misvacasapp/TableSeleccionado.java
@@ -2,21 +2,36 @@ package com.example.misvacasapp;
 
 import java.util.Hashtable;
 
+/**
+ * Clase que crea una tabla hash para saber que elementos han sido seleccionados en la lista
+ * @author Sara Martinez Lopez 
+ * */
 public class TableSeleccionado {
 
+	//Atributos
+	/** Tabla hash de seleccionados*/
 	private Hashtable<Integer, Boolean> seleccionado;
-	private static TableSeleccionado _uniqueInstance;
+//	private static TableSeleccionado _uniqueInstance;
 
+	/** 
+	 * Constructor
+	 * Crea una nueva tabla hash vacia que introduce en el primer valor un entero (el indice en la lista) y el segundo valor un boolean, 
+	 * si esta seleccionado en la vista de la lista se pone un true y si no un false. Al principio todos los valores de la lsta serán false.
+	 * */
 	public TableSeleccionado() {
 		this.seleccionado = new Hashtable<Integer, Boolean>();
 	}
 
-	public static TableSeleccionado getInstance() {
-		if (_uniqueInstance == null)
-			_uniqueInstance = new TableSeleccionado();
-		return _uniqueInstance;
-	}
+//	public static TableSeleccionado getInstance() {
+//		if (_uniqueInstance == null)
+//			_uniqueInstance = new TableSeleccionado();
+//		return _uniqueInstance;
+//	}
 	
+	/**
+	 * Devuelve la tabla hash de seleccionados
+	 * @return HashTable<Integer,Boolean> Tabla hash de seleccionados
+	 * */
 	public Hashtable<Integer, Boolean> getTable(){
 		return seleccionado;
 	}
