a5b779aec375fd7d6bfd4cd120c2c508b71cca7d
sarita43 <sara_c43@hotmail.com>
2015-02-20 12:02:06 +0100

    comentarios y javadoc vacaws

--- webservice/MisVacasWebService/src/web/service/VacaWebService.java
+++ webservice/MisVacasWebService/src/web/service/VacaWebService.java
@@ -4,18 +4,41 @@ import basedatos.*;
 
 public class VacaWebService {
 	
+	/**
+	 * Método que devuelve el animal como String.
+	 * @param id_vaca
+	 *            Id del animal
+	 * @param id_usuario
+	 *            DNI del usuario o Id del usuario
+	 * @return String Vaca como String
+	 * */
 	public String Vaca(String id_vaca,String id_usuario){
 		return new Vaca().vacaString(id_vaca,id_usuario);
 	}
 	
+	/**
+	 * Método que devuelve la lista de animales como String.
+	 * @param id_vaca Id de la vaca
+	 * @return String Lista de medicamentos como String
+	 * */
 	public String listaVacas(String id_usuario){
 		return new Vaca().listaVacasString(id_usuario);
 	}
 	
-	public void a�adirVaca(String vaca) {
-		new Vaca().a�adirVaca(vaca);
+	/**
+	 * Método que añade un animal que se
+	 * pasa por parametro como String. 
+	 * @param vaca Vaca que como String para deserializar
+	 * */
+	public void añadirVaca(String vaca) {
+		new Vaca().añadirVaca(vaca);
 	}
 	
+	/**
+	 * Método que elimina un animal 
+	 * @param id_vaca Id de la vaca
+	 * @param id_usuario Id del usuario
+	 * */
 	public void eliminarVaca(String id_vaca, String id_usuario) {
 		new Vaca().eliminarVaca(id_vaca, id_usuario);
 	}
